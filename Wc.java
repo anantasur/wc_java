24674c3034e17807867da376a07f0089ea0e89b2
Ananthu R V <ananthurv666@hotmail.com>
2015-02-17 15:20:36 +0530

    get number of characters done using test

--- Wc.java
+++ Wc.java
@@ -1,9 +1,20 @@
 public class Wc{
+
   public int getLines(String text){
     if(text.length()==0) return 0;
-    int numberOfLines = 0;
     String[] splittedText = text.split("\\n");
     return(splittedText.length);
+  }
+
+  public int getWords(String text){
+    if(text.length()==0) return 0;
+    String[] splittedText = text.split("\\s");
+    return(splittedText.length);
+  }
+  public int getChars(String text){
+    int delta = getLines(text);
+    if(delta>1) return text.length()+delta;
+    return text.length();
   }
 
 }
--- WcTest.java
+++ WcTest.java
@@ -11,9 +11,66 @@ public class WcTest{
   }
 
   @Test
+  public void getLines_gives_1_for_string_with_space (){
+    String text = " ";
+    Wc wc = new Wc();
+    assertEquals(1,wc.getLines(text));
+  }
+
+  @Test
   public void getLines_gives_2_for_two_lines_present_in_a_string (){
     String text = "hello, this is a good day.\nWhat do you think?\n";
     Wc wc = new Wc();
     assertEquals(2,wc.getLines(text));
   }
+
+  @Test
+    public void getWords_gives_0_for_no_words_in_a_string (){
+    String text = "";
+    Wc wc = new Wc();
+    assertEquals(0,wc.getWords(text));
+  }
+
+  @Test
+    public void getWords_gives_1_for_space_in_a_string (){
+    String text = " ";
+    Wc wc = new Wc();
+    assertEquals(0,wc.getWords(text));
+  }
+
+  @Test
+  public void getWords_gives_1_for_one_word_in_a_string (){
+    String text = "Hello";
+    Wc wc = new Wc();
+    assertEquals(1,wc.getWords(text));
+  }
+
+  @Test
+  public void getChars_gives_0_for_empty_string (){
+    String text = "";
+    Wc wc = new Wc();
+    assertEquals(0,wc.getChars(text));
+  }
+
+  @Test
+  public void getChars_gives_5_for_hello (){
+    String text = "hello";
+    Wc wc = new Wc();
+    assertEquals(5,wc.getChars(text));
+  }
+
+  @Test
+  public void getChars_gives_19_for_hello_How_are_you (){
+    String text = "hello, How are you?";
+    Wc wc = new Wc();
+    assertEquals(19,wc.getChars(text));
+  }
+
+  @Test
+  public void getChars_gives_48_for_hello_How_are_you (){
+    String text = "hello, this is a good day.\nWhat do you think?\n";
+    Wc wc = new Wc();
+    assertEquals(48,wc.getChars(text));
+  }
+
 }
